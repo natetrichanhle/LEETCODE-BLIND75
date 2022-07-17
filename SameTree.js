714c8a203581175fa990bef0ae9ce3b6a94a88c5
Nate Trichanh <natetrichanh@gmail.com>
2022-07-16 18:03:59 -0700

    100. Same Tree

---
+++ SameTree.js
@@ -0,0 +1,10 @@
+var isSameTree = function(p, q) {
+    //     base cases
+        if (p === null && q === null) return true;
+        if (p === null || q === null) return false;
+        
+        if (p.val === q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return false;
+    };
